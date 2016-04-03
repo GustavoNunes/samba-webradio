719b6916f969cf1a047008b911b21dd137c2a04d
Gustavo Nunes <gustavo@gustavo>
2016-04-03 14:32:18 -0300

    Added HTML5 audio function

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,8 +3,23 @@ angular.module('webradio.controllers', [])
     .controller('AppCtrl', function($scope, $ionicModal, $timeout) {})
 
     .controller('RadioCtrl', function($scope) {
+	var audio = null;
+	
 	$scope.webradio = {
-	    url: 'http://104.236.247.184:8000/airtime_128'
+	    url: 'http://104.236.247.184:8000/airtime_128',
+	    play: false
+	};
+
+	$scope.toggleRadio = function() {
+	    if ($scope.webradio.play === true) {
+		audio = new Audio($scope.webradio.url);
+		audio.play();
+	    } else {
+		audio.pause();
+		audio.src = "";
+		audio.load();
+		audio = null;
+	    }	    
 	};
     })
 
